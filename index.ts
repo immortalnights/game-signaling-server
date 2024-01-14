90cd502532a90db194307140938e4b0d652ac115
Andrew <github@seventh.space>
2024-01-14 23:52:34 +0000

    feat: create ws connection to server feat: create RTC connection and send to server

--- index.ts
+++ index.ts
@@ -1,51 +1,190 @@
 import select, { Separator } from "@inquirer/select"
 import input from "@inquirer/input"
+import { WebSocket, createWebSocketStream } from "ws"
+import { RTCPeerConnection } from "werift"
+
+class Client {
+    game?: string
+    ws?: WebSocket
+    rtc?: RTCPeerConnection
+    dc?: RTCDataChannel
+
+    constructor() {}
+
+    async connectToSignalingServer(address: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.ws = new WebSocket(`ws://${address}/`, {})
+
+            const handleFailedToConnect = (err: string) => {
+                console.log("WebSocket error", err)
+                reject(err)
+            }
+
+            this.ws.once("open", () => {
+                console.log("WebSocket opened")
+
+                if (this.ws) {
+                    // this.ws.off("error", handleFailedToConnect)
+                }
+
+                this.ws?.send("ping", () => {})
+
+                setTimeout(() => {
+                    resolve(true)
+                }, 1000)
+            })
+
+            this.ws.on("message", (data, isBinary) => {
+                console.log(
+                    "WebSocket message",
+                    data.toString().length,
+                    isBinary,
+                )
+            })
+
+            this.ws.once("close", (code, reason) => {
+                console.log("WebSocket closed", code, reason)
+            })
+
+            // this.ws.once("error", handleFailedToConnect)
+
+            this.ws.on("error", (err) => {
+                console.log("WebSocket error", err)
+            })
+        })
+    }
+
+    async createGame(): Promise<boolean> {
+        this.game = randomString()
+
+        const pc = new RTCPeerConnection({})
+        pc.iceConnectionStateChange.subscribe((v) =>
+            console.log("pc.iceConnectionStateChange", v),
+        )
+
+        const dc = pc.createDataChannel("game", {
+            protocol: "ttt",
+        })
+
+        dc.stateChanged.subscribe((v) => {
+            console.log("dc.stateChanged", v)
+            if (v === "open") {
+                console.log("open")
+            }
+        })
+
+        let index = 0
+        dc.message.subscribe((data) => {
+            console.log("message", data.toString())
+            dc.send("pong" + index++)
+        })
+
+        const offer = await pc.createOffer()!
+        await pc.setLocalDescription(offer)
+
+        return new Promise((resolve, reject) => {
+            if (this.ws) {
+                const data = JSON.stringify(pc.localDescription)
+                console.log("Sending local description", data.length)
+                this.ws.send(data)
+            } else {
+                console.log("Invalid WebSocket")
+                reject(false)
+            }
+        })
+
+        // const answer = JSON.parse(
+        //     await new Promise((r) =>
+        //         socket.on("message", (data) => r(data as string)),
+        //     ),
+        // )
+        // console.log(answer)
+
+        // await pc.setRemoteDescription(answer)
+    }
+
+    async waitForOpponent(): Promise<boolean> {
+        const check = (timeout: number = 0) => {
+            return false
+        }
+
+        return new Promise((resolve, reject) => {
+            let ok = false
+            let timeout = false
+            const wait = () =>
+                setTimeout(() => {
+                    console.log("Checking...")
+                    if (ok) {
+                        resolve(true)
+                    } else if (timeout) {
+                        reject(false)
+                    } else {
+                        wait()
+                    }
+                }, 1000)
+
+            wait()
+        })
+    }
+
+    async playGame(): Promise<boolean> {
+        return false
+    }
+}
+
+const randomString = (len: number = 6) =>
+    (Math.random() + 1).toString(36).substring(len)
 
 const startGame = async (): Promise<string | undefined> => {
     return undefined
 }
 
-const hostGame = async (): Promise<string | undefined> => {
-    const address = await input({
+const getSignalingServerAddress = async () =>
+    await input({
         message: "Enter Server address",
         default: "127.0.0.1:9001",
     })
 
-    return "abc"
+const hostGame = async (): Promise<Client | undefined> => {
+    const address = await getSignalingServerAddress()
+    const c = new Client()
+    await c.connectToSignalingServer(address)
+    await c.createGame()
+
+    return c
 }
 
-const findGame = async (): Promise<string | undefined> => {
-    const address = await input({
-        message: "Enter Server address",
-        default: "127.0.0.1:9001",
-    })
+// const findGame = async (): Promise<string | undefined> => {
+//     const address = getSignalingServerAddress()
 
-    // fetch games
-    console.log("Loading games...")
+//     await connectToSig(address)
 
-    let game
+//     // fetch games
+//     console.log("Loading games...")
 
-    const answer = await select({
-        message: "Tic-tac-toe: Main Menu",
-        choices: [
-            // TODO list games
-            new Separator(),
-            {
-                name: "Back",
-                value: "back",
-                description: "Back",
-            },
-        ],
-    })
+//     let game
 
-    switch (answer) {
-        case "back": {
-            break
-        }
-    }
+//     const answer = await select({
+//         message: "Tic-tac-toe: Main Menu",
+//         choices: [
+//             // TODO list games
+//             new Separator(),
+//             {
+//                 name: "Back",
+//                 value: "back",
+//                 description: "Back",
+//             },
+//         ],
+//     })
 
-    return game
-}
+//     switch (answer) {
+//         case "back": {
+//             break
+//         }
+//     }
+
+//     return game
+// }
 
 const joinGame = async (game: string) => {}
 const playGame = async (game: string) => {}
@@ -88,18 +227,25 @@ const mainMenu = async () => {
             break
         }
         case "host-game": {
-            const game = await hostGame()
-            if (game) {
-                await playGame(game)
+            const client = await hostGame()
+            if (client) {
+                await client.waitForOpponent()
+                await client.playGame()
             }
+
+            console.log("Game complete or didn't start...")
             break
         }
         case "join-game": {
-            const game = await findGame()
-            if (game) {
-                await joinGame(game)
-                await playGame(game)
-            }
+            const client = new Client()
+            const address = await getSignalingServerAddress()
+            client.connectToSignalingServer(address)
+            // const games = await client.findGames()
+
+            // if (game) {
+            //     await joinGame(game)
+            //     await playGame(game)
+            // }
             break
         }
         case "quit": {
