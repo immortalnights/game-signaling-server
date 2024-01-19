b6673366afb736a293f68eec2140598430bc9600
Andrew <github@seventh.space>
2024-01-19 23:58:13 +0000

    chore: make tests look more like tests

--- tests/tictactoe.test.ts
+++ tests/tictactoe.test.ts
@@ -1,25 +1,45 @@
 import { TicTakToe } from "../src/tictactoe.js"
 
-let t = new TicTakToe()
+const test = (name: string, fn: Function): void => {}
+const expect = (condition: any, value: any): void => {}
 
-t.playerMove(0, 0)
-t.playerMove(1, 4)
-t.playerMove(0, 1)
-t.playerMove(1, 2)
-t.playerMove(0, 6)
-t.playerMove(1, 3)
-t.playerMove(0, 5)
-t.playerMove(1, 8)
-t.playerMove(0, 7)
-t.render()
-console.log("Winner?", t.calculateWinner())
+test("draw", () => {
+    const t = new TicTakToe()
 
-t = new TicTakToe()
+    t.playerMove(0, 0)
+    t.playerMove(1, 4)
+    t.playerMove(0, 1)
+    t.playerMove(1, 2)
+    t.playerMove(0, 6)
+    t.playerMove(1, 3)
+    t.playerMove(0, 5)
+    t.playerMove(1, 8)
+    t.playerMove(0, 7)
 
-t.playerMove(0, 0)
-t.playerMove(1, 4)
-t.playerMove(0, 1)
-t.playerMove(1, 5)
-t.playerMove(0, 2)
-t.render()
-console.log("Winner?", t.calculateWinner())
+    expect(t.calculateWinner(), undefined)
+})
+
+test("naught wins", () => {
+    const t = new TicTakToe()
+
+    t.playerMove(0, 0)
+    t.playerMove(1, 4)
+    t.playerMove(0, 1)
+    t.playerMove(1, 5)
+    t.playerMove(0, 2)
+
+    expect(t.calculateWinner(), "O")
+})
+
+test("naught wins", () => {
+    const t = new TicTakToe()
+
+    t.playerMove(0, 4)
+    t.playerMove(1, 0)
+    t.playerMove(0, 5)
+    t.playerMove(1, 1)
+    t.playerMove(0, 7)
+    t.playerMove(1, 2)
+
+    expect(t.calculateWinner(), "X")
+})
