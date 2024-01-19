a18d29acd811ccc731b9ee9b0d63a391111074e7
Andrew <github@seventh.space>
2024-01-19 23:59:42 +0000

    chore: create PeerConnection class

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,9 @@
 import select, { Separator } from "@inquirer/select"
 import input from "@inquirer/input"
 import { WebSocket, createWebSocketStream } from "ws"
-import {
-    RTCPeerConnection,
-    RTCDataChannel,
-    RTCSessionDescription,
-} from "werift"
-import { randomString } from "./src/utilities.js"
-
-class PeerConnection {
-    pc: RTCPeerConnection
-    private dc?: RTCDataChannel
-
-    constructor() {
-        this.pc = new RTCPeerConnection({})
-        this.pc.iceConnectionStateChange.subscribe((v) =>
-            console.log("pc.iceConnectionStateChange", v),
-        )
-
-        this.pc.onDataChannel.subscribe((channel) => {
-            this.subscribeToDataChannel(channel)
-        })
-    }
-
-    async offer(name: string = "default"): Promise<RTCSessionDescription> {
-        const channel = this.pc.createDataChannel(name, {
-            protocol: "default",
-        })
-        this.subscribeToDataChannel(channel)
-
-        const offer = await this.pc.createOffer()
-        await this.pc.setLocalDescription(offer)
-
-        return offer
-    }
-
-    async response(answer: RTCSessionDescription) {
-        await this.pc.setRemoteDescription(answer)
-    }
-
-    async answer(offer: RTCSessionDescription): Promise<RTCSessionDescription> {
-        await this.pc.setRemoteDescription(offer)
-
-        const answer = await this.pc.createAnswer()
-        await this.pc.setLocalDescription(answer)
-
-        return answer
-    }
-
-    send(data: string | Buffer) {
-        this.dc?.send(data)
-    }
-
-    close() {
-        this.pc.close()
-    }
-
-    private subscribeToDataChannel(channel: RTCDataChannel) {
-        this.dc = channel
-
-        this.dc.stateChanged.subscribe((v) => {
-            console.log("dc.stateChanged", v)
-        })
-
-        this.dc.message.subscribe((data) => {
-            console.log("dc.message", data.toString())
-        })
-
-        this.dc.error.subscribe((err) => console.error)
-    }
-}
+import { RTCSessionDescription } from "werift"
+import { randomString } from "./utilities.js"
+import { PeerConnection } from "./PeerConnection.js"
 
 class Client {
     game?: string
