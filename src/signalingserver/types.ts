b2cdabc6acc8f0de741e39028106866681afe744
Andrew <github@seventh.space>
2024-01-20 16:33:48 +0000

    chore: add message types

--- src/signalingserver/types.ts
+++ src/signalingserver/types.ts
@@ -1,3 +1,10 @@
+export interface MessageInterface {
+    "host-game": Object
+    "list-games": Object
+    "game-list": Object
+    "joined-game": Object
+}
+
 export interface GameRecord {
     id: string
     name: string
@@ -10,4 +17,14 @@ export interface GameOptions {
     name: string
     maxPlayers: number
     [key: string]: unknown
+}
+
+export interface Message {
+    action: keyof MessageInterface
+    data: Object
+}
+
+export interface MessageResponse {
+    action: keyof MessageInterface
+    data: Object
 }
